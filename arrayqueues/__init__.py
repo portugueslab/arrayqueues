521860253f99f184a502c0a222688f34d12da8b3
Luigi Petrucco <luigi.petrucco@gmail.com>
2021-02-17 18:09:33 +0100

    fixed setup.cfg bumpversion

--- arrayqueues/__init__.py
+++ arrayqueues/__init__.py
@@ -7,6 +7,9 @@ __author__ = "Vilim Stih @portugueslab"
 
 __version__ = "1.2.0"
 
-from arrayqueues.shared_arrays import ArrayQueue
-
-# from arrayqueues.portable_queue import PortableQueue
+from arrayqueues.portable_queue import PortableQueue
+from arrayqueues.shared_arrays import (
+    ArrayQueue,
+    IndexedArrayQueue,
+    TimestampedArrayQueue,
+)
