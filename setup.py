7c5909e4f4fd7e559eedcfa540d1e434c99447ff
Luigi Petrucco <luigi.petrucco@gmail.com>
2023-08-02 16:02:42 +0200

    Removed 3.12

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
     ],
     keywords="multiprocessing queues arrays",
     description="Multiprocessing queues for numpy arrays using shared memory",
