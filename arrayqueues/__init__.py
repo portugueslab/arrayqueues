047d1139d28a4d93a3f6e27377ec66912ed2af14
Luigi Petrucco <luigi.petrucco@gmail.com>
2023-08-02 16:06:35 +0200

    Bump version: 1.4.0 → 1.4.1

--- arrayqueues/__init__.py
+++ arrayqueues/__init__.py
@@ -5,7 +5,7 @@ Multiprocessing queues for numpy arrays using shared memory
 
 __author__ = "Vilim Stih @portugueslab"
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 
 from arrayqueues.shared_arrays import (
     ArrayQueue,
